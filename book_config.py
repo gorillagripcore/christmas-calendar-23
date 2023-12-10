e6177c1553b8d8d4b8ab230a2a31e93c191253ed
alexandra <83598143+gorillagripcore@users.noreply.github.com>
2023-12-10 10:34:07 +0100

    Revert "Update book_config.py"
    This reverts commit ccedc48d8f292e43993cb38bd948043c516fd7ab.

--- book_config.py
+++ book_config.py
@@ -1,27 +1,27 @@
 books = [
     {
-        "date": "2023-12-03 10:00",
+        "date": "2023-11-02 10:42",
         "title": "The Little Match Girl by H.C. Andersen",
         "link": "https://www.gordongetty.com/images/documents/174/Little%20Match%20Girl-Text.pdf",
         "image": "books/book1.jpg",
         "description": "On a cold winter night near New Year's, a poor girl tries to sell matches. Afraid to return home with unsold goods, she lights the matches for warmth. Each match reveals comforting visions, contrasting her harsh reality."
     },
     {
-        "date": "2023-12-10 10:00",
+        "date": "2023-11-02 10:43",
         "title": "The Lady with the Dog by Anton Chekhov",
         "link": "https://bcs.bedfordstmartins.com/webpub/english/compclass/Public%20Domain%20Readings/Chekhov%20The%20Lady%20with%20the%20Dog.pdf",
         "image": "books/book2.jpeg",
         "description": "A story of an adulterous affair between an unhappily married Moscow banker and a young married woman that begins while both are vacationing alone in Yalta."
     },
     {
-        "date": "2023-12-17 10:00",
+        "date": "2023-11-02 10:44",
         "title": "Where love is there God is also by Leo Tolstoy",
         "link": "https://www.gutenberg.org/files/38616/38616-h/38616-h.htm",
         "image": "books/book3.jpg",
         "description": "The title refers to the Catholic hymn Ubi Caritas. This short story tells the tale of Martin Avdeitch, who has suffered personal losses and becomes a recluse, engrossed in his Bible."
     },
     {
-        "date": "2023-12-24 10:00",
+        "date": "2023-11-02 10:45",
         "title": "A Kidnapped Santa Claus by L. Frank Baum",
         "link": "https://pinkmonkey.com/dl/library1/digi379.pdf",
         "image": "books/book4.jpg",
